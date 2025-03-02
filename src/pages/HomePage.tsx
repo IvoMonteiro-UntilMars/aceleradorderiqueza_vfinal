0c37d68bcc93e1597c77ff8ba48a28d1ccc8631d
IvoMonteiro-UntilMars <gbr.untilmars@gmail.com>
2025-03-02 23:07:29 +0000

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -36,7 +36,7 @@ function HomePage() {
             </div>
             <div className="relative">
               <img 
-                src="fotoprincipal.jpg" 
+                src="https://sb1xdjxppev-ylkb--5173--31ca1d38.local-credentialless.webcontainer.io/fotoprincipal.jpg" 
                 alt="Ivo Monteiro" 
                 className="rounded-lg shadow-2xl"
               />

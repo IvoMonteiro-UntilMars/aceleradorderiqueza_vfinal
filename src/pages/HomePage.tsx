9861cfae1324225eba5b7b9c88417ee58d16cce8
IvoMonteiro-UntilMars <gbr.untilmars@gmail.com>
2025-03-07 21:06:37 +0000

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,7 +13,7 @@ function HomePage() {
           <div 
             className="absolute inset-0 bg-cover bg-center opacity-15 backdrop-blur-sm"
             style={{
-              backgroundImage: `url('https://lh4.googleusercontent.com/2p0JTRV7AVtlSCSzuxzt0pH1J0j1teJjPLkMVLF2jGmvW9sLJKeUzMyFlgnmf1nEMy0ucOqALB0hMDbydVfbyQ4=w1280')`,
+              backgroundImage: `url('https://lh3.googleusercontent.com/L9xI3h30y0oE8beKEaI-XzSBkA1BEdIaLea14tb_HjlOgUkdmKPtb6Kw85uq2UdTBwjPKeHC_GxqrS81pEhdEm4=w1280')`,
             }}
           ></div>
           <div className="container mx-auto px-4 py-20 grid md:grid-cols-2 gap-12 items-center relative">
